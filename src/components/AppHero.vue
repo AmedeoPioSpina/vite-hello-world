1f8392a77bfe314a23dbb23055c104fd24acf780
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-12-13 12:58:01 +0100

    commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+    <section>
+        <h2>
+          Jumbotron
+        </h2>
+    </section>
+</template>
+
+<style scoped>
+    section {
+        background-color: red;
+        height: 30vh;
+        widows: 80vw;
+        margin: 0 10vw;
+    }
+    h2{
+        text-align: center;
+        vertical-align: ;
+    }
+</style>
